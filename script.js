c0aa8d2d552cfe3f2b5904aee6edf833b4d7f090
Hermanto Prastyawan <hermanto9pras@gmail.com>
2023-08-04 00:31:34 +0700

    push

---
+++ script.js
@@ -0,0 +1,22 @@
+const container = document.querySelector('.container');
+const jumbo = document.querySelector('.jumbo');
+const typeWritter = document.querySelector('.typewritter');
+
+container.addEventListener('click', function(e){
+    if (e.target.className == 'thumb'){
+         // Remove 'active' class from all thumbnails
+         const thumbs = document.querySelectorAll('.thumb');
+         thumbs.forEach(thumb => thumb.classList.remove('active'));
+        jumbo.src = e.target.src;
+        jumbo.classList.add('fade');
+        setTimeout(function(){
+            jumbo.classList.remove('fade');
+        }, 500);
+        e.target.classList.add('active');
+    }
+});
+
+setTimeout(function(){
+    typeWritter.removeChild(typeWritter.lastChild);
+}, 8000);
+
