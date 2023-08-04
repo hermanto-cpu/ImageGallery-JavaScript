a9c5f7a960a76e90f51dab8ecd36cc42371ed00c
Hermanto Prastyawan <hermanto9pras@gmail.com>
2023-08-05 00:44:02 +0700

    update image galeri jadi lebih menarik sedikit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('.container');
 const jumbo = document.querySelector('.jumbo');
 const typeWritter = document.querySelector('.typewritter');
+const bgImage = document.querySelector('.bg-image');
 
 container.addEventListener('click', function(e){
     if (e.target.className == 'thumb'){
@@ -8,9 +9,12 @@ container.addEventListener('click', function(e){
          const thumbs = document.querySelectorAll('.thumb');
          thumbs.forEach(thumb => thumb.classList.remove('active'));
         jumbo.src = e.target.src;
+        bgImage.src = e.target.src;
         jumbo.classList.add('fade');
+        bgImage.classList.add('fade');
         setTimeout(function(){
             jumbo.classList.remove('fade');
+            bgImage.classList.remove('fade');
         }, 500);
         e.target.classList.add('active');
     }
